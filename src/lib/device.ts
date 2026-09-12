338de6be74c5bbd0a307e3b08540b700740ecb34
balajikoppu <balubalaji0110@gmail.com>
2026-09-12 21:09:47 +0530

    refresh

--- src/lib/device.ts
+++
@@ -1,7 +0,0 @@
-export function isMobileDevice() {
-  if (typeof window === "undefined") {
-    return false;
-  }
-
-  return window.matchMedia("(max-width: 768px)").matches;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { ExperienceProvider } from './experience/ExperienceProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ExperienceProvider>
-      <App />
-    </ExperienceProvider>
+    <App />
   </StrictMode>,
 )
